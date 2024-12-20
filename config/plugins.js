4b29d6464b18eb2fa828fdf8cee83ea91c639070
blueprintsPathway <107635376+blueprintsPathway@users.noreply.github.com>
2024-12-20 23:12:53 +0100

    testy

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,20 @@
-module.exports = () => ({});
+// module.exports = () => ({});
+
+module.exports = {
+  documentation: {
+    config: {
+      swagger: {
+        securityDefinitions: {
+          BearerAuth: {
+            type: "apiKey",
+            name: "Authorization",
+            in: "header",
+          },
+        },
+        security: [{ BearerAuth: [] }],
+      },
+    },
+  },
+};
+
+console.log(module.exports);
